448b4c10a05b72fb379248cc7053e22770d408d4
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-11-15 02:02:03 +0400

    Create jssonwork.py

---
+++ jssonwork.py
@@ -0,0 +1,22 @@
+import json
+
+
+class Block():
+
+    def __init__(self, json_block):
+        self.id = json_block['ID']
+        self.name = json_block['Name']
+        self.texture = json_block['Texture']
+        self.props = json_block['Properties']
+
+
+def main():
+    with open('data.json') as f:
+        d = json.load(f)
+
+    b = Block(d)
+    print(b.id, b.name, b.texture, b.props)
+
+
+if __name__ == '__main__':
+    main()
