d0aea1996396ea9b5222def10f0d37c4b918f99b
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-11-21 21:55:38 +0400

    Update asf.cpp

--- asf.cpp
+++ asf.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <complex>
-
-using namespace std;
 
 int main()
 {
-	//complex<int> a;
-	//cin >> a; //(r,i)
-	//cout << abs(a);
 	int a = 4;
 	double b = 1.5;
-	cout << a - int(a) << " " << b - int(b) << endl;
+	std::cout << a - int(a) << " " << b - int(b) << std::endl;
 	return 0;
 }
