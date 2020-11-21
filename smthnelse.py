8ea1c7b98b7a0cbd6f0bdb30fa3ce7b446a63d9c
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-11-21 21:55:55 +0400

    Update smthnelse.py

--- smthnelse.py
+++ smthnelse.py
@@ -1,40 +1,40 @@
-# import time
-# from threading import Thread
+import time
+from threading import Thread
 
 
-# def sleepnprint():
-#     i = 0
-#     while True:
-#         time.sleep(1)
-#         print(i)
-#         i += 1
+def sleepnprint():
+    i = 0
+    while True:
+        time.sleep(1)
+        print(i)
+        i += 1
 
 
-# t1 = Thread(target=sleepnprint)
-# t2 = Thread(target=sleepnprint)
+t1 = Thread(target=sleepnprint)
+t2 = Thread(target=sleepnprint)
 
-# t1.start()
-# t2.start()
-# t1.join()
-# t2.join()
-
-
-def fib(n):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 
 
-def fib1(n):
-    f, s = 0, 1
-    while n > 0:
-        f, s = s, f + s
-        n -= 1
-    return s
+# def fib(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fib(n - 1) + fib(n - 2)
 
 
-for i in range(200):
+# def fib1(n):
+#     f, s = 0, 1
+#     while n > 0:
+#         f, s = s, f + s
+#         n -= 1
+#     return s
+
+
+for i in range(30):
     print(fib1(i), end=' ')
