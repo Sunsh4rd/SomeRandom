c292a0183b24f74306b467b9bc509c29ab0141f0
Sunsh4rd <SsoslickK@gmail.com>
2020-11-21 15:50:36 +0400

    None

--- asf.cpp
+++ asf.cpp
@@ -5,17 +5,11 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-	complex<double> a;
-    cin >> a; //(r,i)
-	cout << abs(a);
-=======
 	//complex<int> a;
 	//cin >> a; //(r,i)
 	//cout << abs(a);
 	int a = 4;
 	double b = 1.5;
 	cout << a - int(a) << " " << b - int(b) << endl;
->>>>>>> 074edb25554c83dad461dfd0c77f21eb3bf92f8c
 	return 0;
 }
