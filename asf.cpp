ab3cc6cc5854ede4e8ff8639db409c7ef88dcb2f
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-11-22 14:58:12 +0400

    Update asf.cpp

--- asf.cpp
+++ asf.cpp
@@ -4,10 +4,21 @@
 int main()
 {
 	std::string alp = "0123456789";
-	int pos, len = 5;
+	n = alp.length() - 1;
+	int pos = 0 , len = 5;
+	std::string word;
+	int aux[len];
 	for (;;)
 	{
 		if (pos >= len)
 			break;
+
+		for (i = 0; i < len; i++)
+			aux[i] = 0;
+
+		for (int i = 0; i < n; ++i)
+		{
+			
+		}
 	}
 }
