91bbe60ce154276f97f61db469b9a92ae112c2fd
Sunsh4rd <SsoslickK@gmail.com>
2020-11-27 21:36:55 +0400

    Gr

--- pys/jssonwork.py
+++ pys/jssonwork.py
@@ -1,22 +1,22 @@
 import json
 
 
-class Block():
+# class Block():
 
-    def __init__(self, json_block):
-        self.id = json_block['ID']
-        self.name = json_block['Name']
-        self.texture = json_block['Texture']
-        self.props = json_block['Properties']
+#     def __init__(self, json_block):
+#         self.id = json_block['ID']
+#         self.name = json_block['Name']
+#         self.texture = json_block['Texture']
+#         self.props = json_block['Properties']
 
 
 def main():
     with open('../srcs/data.json') as f:
         d = json.load(f)
-        print(d)
+        print(type(d))
 
-    b = Block(d)
-    print(b.id, b.name, b.texture, b.props)
+    # b = Block(d)
+    # print(b.id, b.name, b.texture, b.props)
 
 
 if __name__ == '__main__':
