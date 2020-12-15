dca3bf8f9b983ee3ef9f95467fb102032f179cec
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-12-16 03:31:42 +0400

    Update Dxstr.py

--- pys/Dxstr.py
+++ pys/Dxstr.py
@@ -1,32 +1,38 @@
 import math
 from heapq import *
+import json
 
 class Graph():
 
     def __init__(self, gdict = None):
         self.gdict = gdict
 
-
-with open("test_dijkstra.txt") as file: #Читаем файл
-  onstring = file.read().splitlines()
-dict = {}
-dict2 = {}
+with open("graph.json") as f:
+  graph_dict = json.load(f)
+  g = Graph(graph_dict)
 
 
-for item in onstring:
-    key = item.split(" ")[0]
-    l = item.split(" ")[1:]
-    n = item.split(" ")[1:]
-    while len(l) != 0:
-      key2 = l.pop(0)
-      weith = l.pop(0)
-      dict2[key2] = weith
-      dict[key]= dict2
-    dict2 = {}
-print(dict)
-file.close()
+
+# with open("test_dijkstra.txt") as file: #Читаем файл
+#   onstring = file.read().splitlines()
+# dict = {}
+# dict2 = {}
+
+
+# for item in onstring:
+#     key = item.split(" ")[0]
+#     l = item.split(" ")[1:]
+#     n = item.split(" ")[1:]
+#     while len(l) != 0:
+#       key2 = l.pop(0)
+#       weith = l.pop(0)
+#       dict2[key2] = weith
+#       dict[key]= dict2
+#     dict2 = {}
+# print(dict)
+# file.close()
 #Создаем граф
-g = Graph(dict)
+# g = Graph(dict)
 
 def dijkstra(gdict, start):
   verts = list(gdict.keys())
@@ -40,14 +46,15 @@ def dijkstra(gdict, start):
     visited[idx] = True
     if dist[idx] < min_value:
       continue
-    next_verts = list(gdict[idx].keys())
-    for v in next_verts:
-      if visited[v]:
-        continue
-      new_dist = dist[idx] + int(gdict[idx][v])
-      if new_dist < dist[v]:
-        dist[v] = new_dist
-        heappush(queue, (v, new_dist))
+    if gdict[idx]:
+      next_verts = list(gdict[idx].keys())
+      for v in next_verts:
+        if visited[v]:
+          continue
+        new_dist = dist[idx] + int(gdict[idx][v])
+        if new_dist < dist[v]:
+          dist[v] = new_dist
+          heappush(queue, (v, new_dist))
 
   return dist
 
