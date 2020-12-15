fc305b88bf00f9858735f56ecb0f95280e02e2e8
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-12-15 23:46:03 +0400

    Update task8.py

--- pys/task8.py
+++ pys/task8.py
@@ -2,10 +2,10 @@ import pyodbc
 
 def main():
 	cnxn = pyodbc.connect('Driver={SQL Server};'
-						  'Server=DESKTOP-MIRUVK9\SQLEXPRESS;'
+						  'Server=DESKTOP-36HKQFT\SQLEXPRESS;'
 						  'Database=Polyclinic;'
 						  'Trusted_Connection=yes;')
-	
+	#DESKTOP-MIRUVK9\SQLEXPRESS
 	cursor = cnxn.cursor()
 	cursor.execute("SELECT * FROM MedicalHistory")
 
