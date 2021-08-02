a3a0a6308793ba8ad3c80c26d24292a7943966f1
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-08-02 11:53:36 +0400

    Update MusicPlayer.java

--- javs/letsspring/FirstTryFromVideo/src/main/java/music/MusicPlayer.java
+++ javs/letsspring/FirstTryFromVideo/src/main/java/music/MusicPlayer.java
@@ -1,4 +1,12 @@
 package music;
 
 public class MusicPlayer {
+    private Music music;
+
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {
+    }
 }
