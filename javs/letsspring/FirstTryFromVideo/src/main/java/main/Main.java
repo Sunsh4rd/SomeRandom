9a433813ee6e6f4d924f66144947687f0975bdac
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-08-04 18:50:51 +0400

    basic spring stuff from video

--- javs/letsspring/FirstTryFromVideo/src/main/java/main/Main.java
+++ javs/letsspring/FirstTryFromVideo/src/main/java/main/Main.java
@@ -1,7 +1,24 @@
 package main;
 
+import music.Music;
+import music.MusicPlayer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MusicPlayer musicPlayer1 = context.getBean("player", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("player", MusicPlayer.class);
 
+        for (Music music: musicPlayer1.getPlaylist()) {
+            System.out.println(music.getSong());
+        }
+
+        for (Music music: musicPlayer2.getPlaylist()) {
+            System.out.println(music.getSong());
+        }
+
+        System.out.println(musicPlayer1.equals(musicPlayer2));
     }
 }
