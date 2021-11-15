c76c53659c5b056a175dec84d9e6959d1fc66aec
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-11-15 15:06:52 +0400

    4 6

--- studies/kmzi/6.py
+++ studies/kmzi/6.py
@@ -1,22 +1,44 @@
+import math
 import string
+import json
 
 alph = string.ascii_lowercase
-alph_d = {i: alph[i] for i in range(len(alph))}
-alph_dr = {alph[i]: i for i in range(len(alph))}
-
-print(alph_d)
-print(alph_dr)
-
-s1 = {0: 14, 1: 9, 2: 17, 3: 10, 4: 20, 5: 22, 6: 15, 7: 13, 8: 3, 9: 25, 10: 8, 11: 4, 12: 7,
-      13: 23, 14: 1, 15: 0, 16: 6, 17: 11, 18: 12, 19: 18, 20: 16, 21: 24, 22: 21, 23: 2, 24: 5, 25: 19}
-
-s2 = {0: 25, 1: 13, 2: 11, 3: 20, 4: 21, 5: 15, 6: 2, 7: 18, 8: 16, 9: 14, 10: 4, 11: 22, 12: 7,
-      13: 17, 14: 8, 15: 1, 16: 19, 17: 10, 18: 5, 19: 12, 20: 24, 21: 0, 22: 23, 23: 9, 24: 6, 25: 3}
+alph_d = {str(i): alph[i] for i in range(len(alph))}
+alph_dr = {alph[i]: str(i) for i in range(len(alph))}
 
 
-msg = 'messagereally'
+def subs():
+    with open('studies\kmzi\s1.json', 'r') as f:
+        s1 = json.load(f)
 
-ms1 = ''.join(alph_d[s1[alph_dr[m]]] for m in msg)
-ms1ms2 = ''.join(alph_d[s2[alph_dr[m]]] for m in ms1)
- 
-print(ms1, ms1ms2)
+    with open('studies\kmzi\s2.json', 'r') as f:
+        s2 = json.load(f)
+
+    return s1, s2
+
+
+def encrypt(msg, s):
+    return ''.join(alph_d[s[alph_dr[m]]] for m in msg)
+
+
+def compose(s1, s2):
+    return { k: s2[v] for k, v in s1.items()}
+
+
+def main():
+
+    msg = 'messagereally'
+
+    if subs():
+        s1, s2 = subs()
+        print('s1(m) =', encrypt(msg, s1))
+        print('s2(s1(m)) =', encrypt(encrypt(msg, s1), s2))
+        print('s3 = s2(s1)')
+        s3 = compose(s1, s2)
+        print('s3(m) =', encrypt(msg, s3))
+
+    
+
+
+if __name__ == '__main__':
+    main()
