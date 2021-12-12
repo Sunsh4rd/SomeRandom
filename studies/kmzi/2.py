2dc1c7063a82c9dc444f93458d8e4279f89574b7
Sunsh4rd <SsoslickK@gmail.com>
2021-12-12 23:13:41 +0400

    2

--- studies/kmzi/2.py
+++ studies/kmzi/2.py
@@ -1,5 +1,6 @@
+from os import read
 import random
-import pickle
+from typing import Counter
 
 alphabet_s = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 alphabet_d = {alphabet_s[i]: str(i) for i in range(len(alphabet_s))}
@@ -21,7 +22,7 @@ def update_map(s, m, k):
 
 
 def read_text():
-    with open('studies\kmzi\input_4.txt', 'r', encoding='utf-8') as f:
+    with open('studies/kmzi/input_4.txt', 'r', encoding='utf-8') as f:
         return f.read()
 
 
@@ -40,10 +41,49 @@ def decrypt(ciphertext, key):
 
 def main():
     text = read_text()
-    trigrams = split_text_by_kgrams(text, 3)
+    print(text)
+    fr = split_text_by_kgrams(text, 1)
+    bigrams = split_text_by_kgrams(text, 2)
+    print(bigrams)
 
-    # for t in trigrams:
+    srtfrq = ['Ё', 'Ъ', 'Ф', 'Э', 'Щ', 'Ц', 'Ю', 'Ш', 'Ж', 'Х',
+    'Б', 'Ч', 'Й', 'З', 'Г', 'Ы', 'Ь', 'Я', 'У', 'П', 'Л',
+    'М', 'К', 'Д', 'Р', 'В', 'С', 'Т', 'Н', 'Е', 'А', 'И', 'О']
+
+    srtfrq_c = sorted(Counter(fr).items(), key=lambda x:x[1])
+
+    print(srtfrq)
+    print(srtfrq_c)
+
+    
+
+    # for t in bigrams:
         # print(text.count(t))
+
+    mappd_b = {bi: text.count(bi) for bi in bigrams}
+
+    print(mappd_b)
+
+    sorted_mappd_b = sorted(mappd_b.items(), key=lambda x:x[1], reverse=True)
+    print(sorted_mappd_b)
+
+    with open('studies/kmzi/bigr.txt', 'r') as f:
+        a = f.readlines()
+        newl = []
+        for s in a:
+            newl.append(s.replace('\n', '').replace(' ', ''))
+        # print(newl)
+        r_bd = {d[:2]: int(d[2:]) for d in newl}
+
+    # print(r_bd)
+
+    sorted_b = sorted(r_bd.items(), key=lambda x:x[1])
+    print(sorted_b)
+
+    iter_crypt = [x[0].lower() for x in sorted_mappd_b]
+    print(iter_crypt)
+
+    print(''.join(sorted_b[iter_crypt.index(b.lower())][0] for b in bigrams))
 
     # with open('studies\\kmzi\\trigrams', 'rb') as f:
     #     print(pickle.load(f))
