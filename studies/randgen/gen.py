e23c019f8cc893598204e1df9dae3ad170643561
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2022-04-16 03:50:14 +0400

    Update gen.py

--- studies/randgen/gen.py
+++ studies/randgen/gen.py
@@ -33,11 +33,22 @@ class Generator():
         values = []
         n, p, s, m, js, y1p = iv.values()
         x_init = [int(d) for d in bin(y1p)[2:]]
-        a_s = [1 if i+1 in js else 0 for i in range(p)]
-        xs = []
-        for i in range(5):
-            pass
-        return a_s, x_init
+        x_init = [0] * (p - len(x_init)) + x_init
+        # print(x_init)
+        x_all = x_init[:]
+        for i in range(n):
+            for _ in range(p):
+                next_bit = 0
+                for jt in js:
+                    next_bit ^= x_init[jt-1]
+                x_init.append(next_bit)
+                x_all.append(next_bit)
+                x_init.pop(0)
+            values.append(int(''.join(str(x) for x in x_init)[:s], 2))
+            x_init = x_init[s:] + x_all[i*s:s-len(x_init[s:])]
+            print(x_init)
+                
+        return x_init, js, values
 
 
 def main():
@@ -51,7 +62,7 @@ def main():
     print(Generator.linear_congruential(n=10, a=3, c=10, m=64, x0=1))
     print(Generator.additive(n=10, s=3, a_s=[
           1, 3, 5], xs=[2, 4, 6], c=7, m=93))
-    print(Generator.lfsr(n=None, p=5, s=None, m=None, js=[1, 5], y1p=12))
+    print(Generator.lfsr(n=25, p=12, s=7, m=3, js=[1, 3, 8], y1p=992))
 
 
 if __name__ == '__main__':
