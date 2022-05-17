9d4bb89dab791b86ba4241aee8fc91612a8e4c10
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2022-05-17 05:00:41 +0400

    rsa

--- studies/kmzi/8sem/gen_digits.py
+++ studies/kmzi/8sem/gen_digits.py
@@ -12,26 +12,19 @@ def miller_rabin(n, k):
         return True
 
     n1 = (n - Bigint([1]))[0]
-    # print(n)
     q = n1
     t = None
     s = 0
     while True:
         q, r = divmod(q, Bigint([2]))
-        # print(q, r)
         s += 1
         if divmod(q, Bigint([2]))[1] != Bigint([0]):
             t = q
-            # print(t)
             break
 
-    # print('s=', s)
     for _ in range(k):
         a = Bigint(int_to_list_of_digits(random.randint(2, int(str(n))-2)))
-        # print(a, t, n)
         x = pow(a, t, n)
-        # print(a, t, x)
-        # print(type(x))
         if x == Bigint([1]) or x == n1:
             continue
 
@@ -53,7 +46,6 @@ def miller_rabin(n, k):
 def gen_digits(k):
     halfk = k // 2
     x1, x2 = 1 << halfk-1, (1 << halfk) - 1
-    # print('xs', x1, x2)
     it = 0
     while True:
         if it == x2-x1:
@@ -62,20 +54,15 @@ def gen_digits(k):
         q = Bigint(int_to_list_of_digits(random.randint(x1, x2)))
         s = Bigint(int_to_list_of_digits(random.randint(x1, x2)))
         p = q*s + Bigint([1])
-        # print('p', p, s, q)
         if divmod(q, Bigint([2]))[1] == Bigint([0]):
-            # print('q even')
             continue
         if q == Bigint([1]):
-            # print('q 1')
             continue
         if miller_rabin(q, int(math.log2(int(str(q))))) and divmod(s, Bigint([2]))[1] == Bigint([0]) and p < (Bigint([2])*q + Bigint([1])) * (Bigint([2])*q + Bigint([1])) and pow(Bigint([2]), q*s, p) == Bigint([1]) and pow(Bigint([2]), s, p) != Bigint([1]):
-            # print(f'p = {p}, q = {q}, s = {s}')
             return p
 
 
 def main():
-    # print(miller_rabin(Bigint([3]), 2))
     k = int(input('k = '))
     print(gen_digits(k))
 
