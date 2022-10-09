0a9db3c0d5ee35459dcebb4ea6cec91f73ea55fb
Sunsh4rd <SsoslickK@gmail.com>
2022-10-10 03:18:30 +0400

    nn

--- studies/neuralnets/testre.py
+++ studies/neuralnets/testre.py
@@ -15,13 +15,11 @@ class Edge:
 
 
 def main():
-    print(re.findall(r'\(\s*\w+\s*,\s*\w+\s*,\s*[1-9]{1}\d*\s*\)\s*[,]?',
-          '(v1,v3,1)   ,(v1,v2,1),   (v2, v3, a), (v1, v2, 0),(v1, v2,2)'))
     with open('graph.txt', 'r', encoding='utf-8') as g_in:
-        print(
-            # re.match(r'([\(\w+\s*\,\s*\w+\s*\,\s*\d+\)\s,\s])*\)', g_in.read()))
-            # re.match(r'[\(\w+\s*\,\s*\w+\s*\,\s*\d+\)]*', g_in.read()))
-            re.fullmatch(r'[\(\w+\, \w+\, \d+\)]*', g_in.read()))
+        g = g_in.read()
+        if re.fullmatch(r'(\(\s*\w+\s*,\s*\w+\s*,\s*[1-9]{1}\d*\s*\)\s*[,]?\s*)*', g):
+            print(re.findall(
+                r'\(\s*\w+\s*,\s*\w+\s*,\s*[1-9]{1}\d*\s*\)\s*[,]?', g))
 
 
 if __name__ == '__main__':
