05e209f7119c5bbc248fbdae2ea52c1af9dcaeea
Sunsh4rd <SsoslickK@gmail.com>
2022-11-14 15:05:42 +0400

    pasec 2

--- studies/pasec/2.py
+++ studies/pasec/2.py
@@ -1,2 +1,24 @@
-with open('/dev/sdc1', 'rb') as f:
-    print(f.read(2048))
+from os import popen
+
+
+def main():
+    mount = popen(f'df {__file__}').readlines()[1]
+    mount = mount[:mount.index(' ')]
+    print(mount)
+    with open(mount, 'rb') as f:
+        print(f.read(128))
+    print(int('0x1e0', base=16))
+    skip = 4096
+    while True:
+        free_space = popen(f'sudo xxd -s {skip} -l 512 {mount}').readlines()
+        for i in free_space:
+            print(i[:8],i[10:49])
+            if i[10:49] != '0000 0000 0000 0000 0000 0000 0000 0000':
+                skip += 512
+                break
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
