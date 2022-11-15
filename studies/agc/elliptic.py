2e4482edf8b20b46debda9c46b9c3a1017fa752f
Sunsh4rd <SsoslickK@gmail.com>
2022-11-15 19:32:04 +0400

    format elliptic

--- studies/agc/elliptic.py
+++ studies/agc/elliptic.py
@@ -66,21 +66,22 @@ def verify_n(p, a, b):
             r = n // 2
             if isprime(r):
                 # print(r)
-                opts.append((n,r,2))
+                opts.append((n, r, 2))
         if n % 4 == 0:
             # print(n)
             r = n // 4
             if isprime(r):
                 # print(r)
-                opts.append((n,r,4))
+                opts.append((n, r, 4))
     print(opts)
     if len(opts) == 0:
         return None
     return choice(opts)
 
 
-def verify_p(p,r,m):
-    return p != r and bool(sum((pow(p,i,r) != 1 for i in range(1,m+1))))
+def verify_p(p, r, m):
+    return p != r and bool(sum((pow(p, i, r) != 1 for i in range(1, m+1))))
+
 
 def legendre(a, p):
     res = pow(a, (p-1)//2, p)
@@ -91,7 +92,7 @@ def legendre(a, p):
     # if a > p or a < 0:
     #     r = a % p
     #     return legendre(r,p)
-    
+
     # if 0 < a < p
 
     # if a == 1:
@@ -101,29 +102,31 @@ def legendre(a, p):
     # if a % 2 == 1 and a != 1:
     #     return legendre(p % a, a) * (-1) ** ((a - 1) * ((p - 1) // 4))
 
-def gen_point(p,n,r):
+
+def gen_point(p, n, r):
     # a, b = factorize(65129)
     # n,r,k = verify_n(65129, a, b) # verify_n(p,a,b)
     # print(n,r)
     # while True:
-        # x0, y0 = randint(1,p-1), randint(1,p-1)
-    for x0 in range(1,p):
-        for y0 in range(2,p):
-            a_c = (((pow(y0,2,p) - pow(x0,3,p)) % p) * pow(x0,-1,p)) % p
+    # x0, y0 = randint(1,p-1), randint(1,p-1)
+    for x0 in range(1, p):
+        for y0 in range(2, p):
+            a_c = (((pow(y0, 2, p) - pow(x0, 3, p)) % p) * pow(x0, -1, p)) % p
             if n == 2*r:
                 print(2, a_c)
                 if legendre(a_c, p) == -1:
-                    print(a_c,'not')
+                    print(a_c, 'not')
                     break
             if n == 4*r:
                 print(4, a_c)
-                if legendre(a_c,p) == 1:
+                if legendre(a_c, p) == 1:
                     print('is')
                     break
         break
     else:
         return None, None, None
     return x0, y0, a_c
+
 
 def pp(x1, y1, x2, y2, a, p):
     if x1 == x2 and -y1 % p == y2:
@@ -136,6 +139,7 @@ def pp(x1, y1, x2, y2, a, p):
     x3 = (pow(alpha, 2, p)-x1-x2) % p
     y3 = (alpha*(x1-x3) - y1) % p
     return x3, y3
+
 
 def main():
     m = 5
@@ -152,23 +156,23 @@ def main():
         n = ver_n[0]
         r = ver_n[1]
         print(r)
-        ver_p = verify_p(p,r,m)
+        ver_p = verify_p(p, r, m)
         print(ver_p)
         if not ver_p:
             continue
         break
-        
+
     print(p, ver_n, ver_p, 'steps 1-4 ok')
-    
+
     while True:
-        x0,y0, a_c = gen_point(p, n,r)
-        print(x0,y0)
+        x0, y0, a_c = gen_point(p, n, r)
+        print(x0, y0)
         if x0 is None and y0 is None:
             continue
-        p0 = (x0,y0)
+        p0 = (x0, y0)
         for _ in range(n):
             try:
-                np = pp(*p0,x0, y0, a_c, p)
+                np = pp(*p0, x0, y0, a_c, p)
                 p0 = (np[0], np[1])
                 print(p0)
             except Exception as e:
@@ -177,7 +181,6 @@ def main():
         print(f'mod {p}, p0 = {x0},{y0}, pinf = {p0}')
         break
 
-    
 
 if __name__ == '__main__':
     main()
