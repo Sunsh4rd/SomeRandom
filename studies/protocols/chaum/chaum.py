f017e2f9b6f1560d78d3cd44e92cd8cbbcb66e7e
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2022-11-21 18:18:13 +0400

    chaum

--- studies/protocols/chaum/chaum.py
+++ studies/protocols/chaum/chaum.py
@@ -1,22 +1,27 @@
 from json import dump, load
 from math import gcd
-from random import getrandbits
-from sympy import isprime
+from random import getrandbits, randint
+from sympy import isprime, factorint
 
 
-def generator(p):
-    fact = []
+def get_primitive_root(p):
+    # fact = []
+    # phi = p-1
+    # n = phi
+    # for i in range(2, int(n**0.5)+1):
+    #     if n % i == 0:
+    #         fact.append(i)
+    #         while n % i == 0:
+    #             n //= i
+
+    # if n > 1:
+    #     fact.append(n)
     phi = p-1
-    n = phi
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            fact.append(i)
-            while n % i == 0:
-                n //= i
-
-    if n > 1:
-        fact.append(n)
-
+    fact = factorint(phi)
+    print(fact)
+    fact_l = [[k]*v for k,v in fact.items()]
+    print(fact_l)
+    
     for res in range(2, p+1):
         ok = True
         i = 0
@@ -29,13 +34,19 @@ def generator(p):
 
 
 def gen_shared_params():
-    l = int(input('length of prime: '))
-    p = getrandbits(l)
+    l = int(input('Длина числа p: '))
+    pi = getrandbits(l)
+    p = pi
+    k = 0
     while True:
         if isprime(p):
             break
-        p = getrandbits(l)
-    g = generator(p)
+        k += 1
+        # q = get_random_prime(16)
+        p = (2**k) * pi + 1
+    print(p)
+    g = get_primitive_root(p)
+    print(f'p = {p}, g = {g}')
     with open('chaum_parameters/p.json', 'w', encoding='utf-8') as pw:
         dump({'p': p}, pw, indent=4)
     with open('chaum_parameters/g.json', 'w', encoding='utf-8') as gw:
@@ -43,7 +54,7 @@ def gen_shared_params():
 
 
 def gen_private_params():
-    l = int(input('length of x: '))
+    l = int(input('Длина числа x: '))
     x = getrandbits(l)
     with open('chaum_parameters/p.json', 'r', encoding='utf-8') as pr:
         p = load(pr)['p']
@@ -54,6 +65,7 @@ def gen_private_params():
             break
         x = getrandbits(l)
     y = pow(g, x, p)
+    print(f'x = {x}, y = {y}')
     with open('chaum_parameters/public_key.json', 'w', encoding='utf-8') as pubw:
         dump({'p': p, 'g': g, 'y': y}, pubw, indent=4)
     with open('chaum_parameters/private_key.json', 'w', encoding='utf-8') as privw:
@@ -61,44 +73,62 @@ def gen_private_params():
 
 
 def sign():
-    me = 'message'
+    with open('chaum_parameters/to_sign.json', 'r', encoding='utf-8') as tsr:
+        me = load(tsr)['message']
     m = hash(me)
     with open('chaum_parameters/p.json', 'r', encoding='utf-8') as pr:
         p = load(pr)['p']
     with open('chaum_parameters/private_key.json', 'r', encoding='utf-8') as privr:
         x = load(privr)['x']
     sign = pow(m, x, p)
+    print(f'z = {sign}')
     with open('chaum_parameters/signed.json', 'w', encoding='utf-8') as signedw:
-        dump({'message': m, 'signature': sign}, signedw, indent=4)
+        dump({'message': me, 'signature': sign}, signedw, indent=4)
 
 
 def verify():
-    la = int(input('length of a: '))
-    lb = int(input('length of b: '))
-    a = getrandbits(la)
-    b = getrandbits(lb)
-    print('a', a)
-    print('b', b)
     with open('chaum_parameters/signed.json', 'r', encoding='utf-8') as signedr:
         data = load(signedr)
-        m, z = data['message'], data['signature']
+        m, z = hash(data['message']), data['signature']
     with open('chaum_parameters/public_key.json', 'r', encoding='utf-8') as pubr:
         data = load(pubr)
         p, g, y = data['p'], data['g'], data['y']
+
+    a, b = randint(2, p-1), randint(2, p-1)
+    print(f'a = {a}, b = {b}')
+    with open('chaum_parameters/ab.json', 'w', encoding='utf-8') as abw:
+        dump({'a': a, 'b': b}, abw, indent=4)
     c = (pow(z, a, p) * pow(y, b, p)) % p
+    print(f'c = {c}')
+    with open('chaum_parameters/c.json', 'w', encoding='utf-8') as cw:
+        dump({'c': c}, cw, indent=4)
     with open('chaum_parameters/private_key.json', 'r', encoding='utf-8') as privr:
         x = load(privr)['x']
     t = pow(x, -1, p-1)
+    with open('chaum_parameters/t.json', 'w', encoding='utf-8') as tw:
+        dump({'t': t}, tw, indent=4)
     d = pow(c, t, p)
-    print('verified' if d % p == (pow(m, a, p)*pow(g, b, p)) %
-          p else 'not verified')
+    with open('chaum_parameters/d.json', 'w', encoding='utf-8') as dw:
+        dump({'d': d}, dw, indent=4)
+    print(f't = {t}, d = {d}')
+    print('Подпись подлинная' if d % p == (pow(m, a, p)*pow(g, b, p)) %
+          p else 'Подпись не подлинная')
 
 
 def main():
-    gen_shared_params()
-    gen_private_params()
-    sign()
-    verify()
+    while True:
+        opt = input(
+            '0 - Генерация общих параметров\n1 - Генерация индивидуальных параметров\n2 - Генрация подписи\n3 - Проверка подписи\n')
+        if opt == '0':
+            gen_shared_params()
+        elif opt == '1':
+            gen_private_params()
+        elif opt == '2':
+            sign()
+        elif opt == '3':
+            verify()
+        else:
+            break
 
 
 if __name__ == '__main__':
