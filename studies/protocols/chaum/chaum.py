b7627e252ce3380ea600a44e96ab4aea95fb121d
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2022-11-28 17:43:40 +0400

    suka pidor

--- studies/protocols/chaum/chaum.py
+++ studies/protocols/chaum/chaum.py
@@ -1,51 +1,101 @@
+from itertools import chain
 from json import dump, load
 from math import gcd
-from random import getrandbits, randint
+from random import getrandbits, randint, randrange
 from sympy import isprime, factorint
 
 
-def get_primitive_root(p):
-    # fact = []
-    # phi = p-1
-    # n = phi
-    # for i in range(2, int(n**0.5)+1):
-    #     if n % i == 0:
-    #         fact.append(i)
-    #         while n % i == 0:
-    #             n //= i
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349]
 
-    # if n > 1:
-    #     fact.append(n)
-    phi = p-1
-    fact = factorint(phi)
-    print(fact)
-    fact_l = [[k]*v for k,v in fact.items()]
-    print(fact_l)
-    
-    for res in range(2, p+1):
-        ok = True
-        i = 0
-        while i < len(fact) and ok:
-            ok &= pow(res, phi // fact[i], p) != 1
-            i += 1
-        if ok:
-            return res
-    return None
+
+def n_bit_random(n):
+    return randrange(2**(n-1)+1, 2**n - 1)
+
+
+def get_low_level_prime(n):
+    while True:
+        pc = n_bit_random(n)
+        for divisor in first_primes_list:
+            if pc % divisor == 0 and divisor**2 <= pc:
+                break
+        else:
+            return pc
+
+
+# def is_miller_rabin_passed(mrc):
+#     maxDivisionsByTwo = 0
+#     ec = mrc-1
+#     while ec % 2 == 0:
+#         ec >>= 1
+#         maxDivisionsByTwo += 1
+#     assert(2**maxDivisionsByTwo * ec == mrc-1)
+
+#     def trialComposite(round_tester):
+#         if pow(round_tester, ec, mrc) == 1:
+#             return False
+#         for i in range(maxDivisionsByTwo):
+#             if pow(round_tester, 2**i * ec, mrc) == mrc-1:
+#                 return False
+#         return True
+
+#     # Set number of trials here
+#     numberOfRabinTrials = 20
+#     for i in range(numberOfRabinTrials):
+#         round_tester = randrange(2, mrc)
+#         if trialComposite(round_tester):
+#             return False
+#     return True
+
+
+# def get_primitive_root(p):
+#     phi = p-1
+#     fact = factorint(phi)
+#     # print(fact)
+#     fact_l = [[k]*v for k, v in fact.items()]
+#     fact_l = list(chain(*fact_l))
+#     # print(fact_l)
+
+#     for res in range(2, p+1):
+#         ok = True
+#         i = 0
+#         while i < len(fact_l) and ok:
+#             ok &= pow(res, phi // fact_l[i], p) != 1
+#             i += 1
+#         if ok:
+#             return res
+#     return None
+
+
+def check_element_is_primitive(element, p, q):
+    return not pow(element, (p - 1) // q, p) == 1 and not pow(element, (p - 1) // 2, p) == 1
+
+
+def get_g(p, q):
+    g = randint(1, p - 1)
+    while not check_element_is_primitive(g, p, q):
+        g = randint(1, p - 1)
+    return g
 
 
 def gen_shared_params():
     l = int(input('Длина числа p: '))
-    pi = getrandbits(l)
-    p = pi
+    q = get_low_level_prime(l // 2)
     k = 0
+    p = (2**k) * q + 1
     while True:
         if isprime(p):
             break
         k += 1
-        # q = get_random_prime(16)
-        p = (2**k) * pi + 1
-    print(p)
-    g = get_primitive_root(p)
+        p = (2**k) * q + 1
+    g = get_g(p, q)
     print(f'p = {p}, g = {g}')
     with open('chaum_parameters/p.json', 'w', encoding='utf-8') as pw:
         dump({'p': p}, pw, indent=4)
@@ -54,16 +104,17 @@ def gen_shared_params():
 
 
 def gen_private_params():
-    l = int(input('Длина числа x: '))
-    x = getrandbits(l)
+    # l = int(input('Длина числа x: '))
     with open('chaum_parameters/p.json', 'r', encoding='utf-8') as pr:
         p = load(pr)['p']
     with open('chaum_parameters/g.json', 'r', encoding='utf-8') as gr:
         g = load(gr)['g']
+
+    x = randint(2, p-1)
     while True:
         if gcd(x, p-1) == 1:
             break
-        x = getrandbits(l)
+        x = randint(2, p-1)
     y = pow(g, x, p)
     print(f'x = {x}, y = {y}')
     with open('chaum_parameters/public_key.json', 'w', encoding='utf-8') as pubw:
@@ -73,8 +124,9 @@ def gen_private_params():
 
 
 def sign():
-    with open('chaum_parameters/to_sign.json', 'r', encoding='utf-8') as tsr:
-        me = load(tsr)['message']
+    with open('chaum_parameters/to_sign.txt', 'r', encoding='utf-8') as tsr:
+        # me = load(tsr)['message']
+        me = tsr.read()
     m = hash(me)
     with open('chaum_parameters/p.json', 'r', encoding='utf-8') as pr:
         p = load(pr)['p']
@@ -82,18 +134,19 @@ def sign():
         x = load(privr)['x']
     sign = pow(m, x, p)
     print(f'z = {sign}')
-    with open('chaum_parameters/signed.json', 'w', encoding='utf-8') as signedw:
-        dump({'message': me, 'signature': sign}, signedw, indent=4)
+    with open('chaum_parameters/signed.txt', 'w', encoding='utf-8') as signedw:
+        signedw.write(f'{sign}')
+        # dump({'message': me, 'signature': sign}, signedw, indent=4)
 
 
-def verify():
-    with open('chaum_parameters/signed.json', 'r', encoding='utf-8') as signedr:
-        data = load(signedr)
-        m, z = hash(data['message']), data['signature']
+def verify3():
+    with open('chaum_parameters/signed.txt', 'r', encoding='utf-8') as signedr:
+        data = signedr.read().split()
+        z = int(data[-1])
+        print(z)
     with open('chaum_parameters/public_key.json', 'r', encoding='utf-8') as pubr:
         data = load(pubr)
         p, g, y = data['p'], data['g'], data['y']
-
     a, b = randint(2, p-1), randint(2, p-1)
     print(f'a = {a}, b = {b}')
     with open('chaum_parameters/ab.json', 'w', encoding='utf-8') as abw:
@@ -102,6 +155,20 @@ def verify():
     print(f'c = {c}')
     with open('chaum_parameters/c.json', 'w', encoding='utf-8') as cw:
         dump({'c': c}, cw, indent=4)
+
+    with open('chaum_parameters/to_sign.txt', 'r', encoding='utf-8') as msgr:
+        me = msgr.read()
+        m = hash(me)
+        print(me, m)
+
+    print('Подпись подлинная' if d % p == (pow(m, a, p)*pow(g, b, p)) %
+          p else 'Подпись не подлинная')
+
+
+def verify4():
+    with open('chaum_parameters/public_key.json', 'r', encoding='utf-8') as pubr:
+        data = load(pubr)
+        p, g, y = data['p'], data['g'], data['y']
     with open('chaum_parameters/private_key.json', 'r', encoding='utf-8') as privr:
         x = load(privr)['x']
     t = pow(x, -1, p-1)
@@ -111,14 +178,16 @@ def verify():
     with open('chaum_parameters/d.json', 'w', encoding='utf-8') as dw:
         dump({'d': d}, dw, indent=4)
     print(f't = {t}, d = {d}')
-    print('Подпись подлинная' if d % p == (pow(m, a, p)*pow(g, b, p)) %
-          p else 'Подпись не подлинная')
+
+
+def verify5():
+    pass
 
 
 def main():
     while True:
         opt = input(
-            '0 - Генерация общих параметров\n1 - Генерация индивидуальных параметров\n2 - Генрация подписи\n3 - Проверка подписи\n')
+            '0 - Генерация общих параметров\n1 - Генерация индивидуальных параметров\n2 - Генрация подписи\n3 - Проверка подписи (шаг 3)\n4 - Проверка подписи (шаг 4)\n5 - Проверка подписи (шаг 5)\n')
         if opt == '0':
             gen_shared_params()
         elif opt == '1':
@@ -126,7 +195,11 @@ def main():
         elif opt == '2':
             sign()
         elif opt == '3':
-            verify()
+            verify3()
+        elif opt == '4':
+            verify4()
+        elif opt == '5':
+            verify5()
         else:
             break
 
