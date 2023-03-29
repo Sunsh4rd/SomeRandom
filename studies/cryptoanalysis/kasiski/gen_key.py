86dabbb1c72f096c4ab51868f2372927c1c3bf97
Sunsh4rd <SsoslickK@gmail.com>
2023-03-30 00:37:43 +0400

    permutation

--- studies/cryptoanalysis/kasiski/gen_key.py
+++ studies/cryptoanalysis/kasiski/gen_key.py
@@ -4,5 +4,8 @@ from random import shuffle
 def gen_key(n):
     r = list(range(n))
     shuffle(r)
+    print(r)
+    while r[0] != 0:
+        r.append(r.pop(0))
     with open('params/key.txt', 'w', encoding='utf-8') as key_w:
         key_w.write(' '.join(str(i) for i in r))
--- studies/cryptoanalysis/kasiski/kasiski.py
+++ studies/cryptoanalysis/kasiski/kasiski.py
@@ -4,18 +4,24 @@ from decrypt import decrypt
 
 
 def main():
-    while True:
-        opt = input(
-            '0 - Генерация ключа моноциклической перестановки\n1 - Зашифровать\n2 - Расшифровать\n')
-        if opt == '0':
-            n = int(input('Длина перестановки: '))
-            gen_key(n)
-        elif opt == '1':
-            encrypt()
-        elif opt == '2':
-            decrypt()
-        else:
-            break
+    block = [''] * 5
+    print(block)
+    ts = 'abcde'
+    block[0] = ts[4]
+    block[2] = ts[1]
+    print(block)
+    # while True:
+    #     opt = input(
+    #         '0 - Генерация ключа моноциклической перестановки\n1 - Зашифровать\n2 - Расшифровать\n')
+    #     if opt == '0':
+    #         n = int(input('Длина перестановки: '))
+    #         gen_key(n)
+    #     elif opt == '1':
+    #         encrypt()
+    #     elif opt == '2':
+    #         decrypt()
+    #     else:
+    #         break
 
 
 if __name__ == '__main__':
