89dc0ed17422d25109053125ee49731efabe78e2
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-04-17 18:28:00 +0400

    tinkoff contest

---
+++ javs/Contest/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        "Мобайл"
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int c = scanner.nextInt();
+//        int d = scanner.nextInt();
+//        System.out.println(d > b ? a + (d - b) * c : a);
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if ((n & (n - 1)) == 0) {
+            
+        }
+    }
+}
