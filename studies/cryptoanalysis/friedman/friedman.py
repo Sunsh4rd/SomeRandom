bc73b99d8bb806f85a4ee063536019df21d57302
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-04-19 21:49:41 +0400

    friedman

--- studies/cryptoanalysis/friedman/friedman.py
+++ studies/cryptoanalysis/friedman/friedman.py
@@ -1,5 +1,18 @@
+import string
+
+
+def index_of_coincidence_avg(seq1, seq2):
+    alph = string.ascii_lowercase
+    return sum(seq1.count(a) / len(seq1) * seq2.count(a) / len(seq2) for a in alph)
+
+
+def index_of_coincidence(seq1, seq2):
+    return len(list(filter(lambda pair: pair[0] == pair[1], zip(seq1, seq2)))) / min(len(seq1), len(seq2))
+
+
 def main():
-    pass
+    print(index_of_coincidence('bbcdf', 'abcdef'))
+    print(index_of_coincidence_avg('the', 'the'))
 
 
 if __name__ == '__main__':
