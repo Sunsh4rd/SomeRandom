bf34ceb36af3c5ad06618d3bcd0bf19c39317296
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-05-08 23:36:56 +0400

    update

--- javs/apartments/src/main/java/pro/java/apartments/ApartmentsApplication.java
+++ javs/apartments/src/main/java/pro/java/apartments/ApartmentsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApartmentsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApartmentsApplication.class, args);
     }
-
 }
