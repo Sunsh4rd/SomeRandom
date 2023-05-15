ae743eb4a7f9ba707cb3711e55d303125ce8875e
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-05-16 01:29:46 +0400

    Update Task4.java

--- javs/Contest/src/Task4.java
+++ javs/Contest/src/Task4.java
@@ -1,5 +1,18 @@
+import java.io.File;
+import java.nio.file.Files;
+
 public class Task4 {
     public static void main(String[] args) {
+        System.out.println(method1() & method2());
+    }
 
+    static boolean method1() {
+        System.out.print("method1 ");
+        return false;
+    }
+
+    static boolean method2() {
+        System.out.print("method2 ");
+        return true;
     }
 }
