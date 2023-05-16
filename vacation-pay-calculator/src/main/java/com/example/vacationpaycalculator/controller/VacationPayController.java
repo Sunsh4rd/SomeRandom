d2931c68f6e83f35bafe55b85d12418c5bfc7d98
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-05-16 12:58:01 +0400

    vacation pay

--- vacation-pay-calculator/src/main/java/com/example/vacationpaycalculator/controller/VacationPayController.java
+++ vacation-pay-calculator/src/main/java/com/example/vacationpaycalculator/controller/VacationPayController.java
@@ -2,17 +2,24 @@ package com.example.vacationpaycalculator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class VacationPayController {
 
     @GetMapping("/calculate")
     public String getVacationPay(
-            @RequestParam Integer salary,
-            @RequestParam Integer vacationDays
+//            @RequestParam Integer salary,
+//            @RequestParam Integer vacationDays
     ) {
 //        return salary / (int) 29.3 * vacationDays;
-        return "calculation";
+        return "calculate";
+    }
+
+    @PostMapping("/calculate")
+    @ModelAttribute("calculate")
+    public String vacate() {
+        return "ye";
     }
 }
