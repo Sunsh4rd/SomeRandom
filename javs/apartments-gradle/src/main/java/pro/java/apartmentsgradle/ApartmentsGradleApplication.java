616faae9d54a7a8604880980846bc07c1ec51eeb
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-05-19 01:40:03 +0400

    aparatments gradle

---
+++ javs/apartments-gradle/src/main/java/pro/java/apartmentsgradle/ApartmentsGradleApplication.java
@@ -0,0 +1,13 @@
+package pro.java.apartmentsgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentsGradleApplication.class, args);
+	}
+
+}
---
+++ javs/apartments-gradle/src/test/java/pro/java/apartmentsgradle/ApartmentsGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.java.apartmentsgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentsGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
