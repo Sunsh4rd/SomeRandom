78d3c712fb8005b1ebce5b7c51a6ac61bf3c0472
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-05-23 05:05:59 +0400

    regex

--- javs/JavaProgramming/src/regexdemo/Main.java
+++ javs/JavaProgramming/src/regexdemo/Main.java
@@ -3,13 +3,38 @@ package regexdemo;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        var openingTagExpr = "<[a-zA-Z]+[^>]*>";
-        try(var html = Files.lines(Paths.get("test.html"))) {
-            html.filter(s -> s.matches(openingTagExpr))
-                    .forEach(System.out::println);
+        var openingTags = new ArrayList<String>();
+        var closingTags = new ArrayList<String>();
+        var openingTagExpr = Pattern.compile("<(\\w+)[^>]*>");
+        var closingTagExpr = Pattern.compile("</(\\w+)>");
+        try {
+            var html = Files.readString(Paths.get("test.html"));
+            System.out.println(html);
+            var matcherOpening = openingTagExpr.matcher(html);
+            var matcherClosing = closingTagExpr.matcher(html);
+            while (matcherOpening.find()) {
+                openingTags.add(matcherOpening.group(1));
+            }
+            while (matcherClosing.find()) {
+                closingTags.add(matcherClosing.group(1));
+            }
+            openingTags.sort(String::compareTo);
+            closingTags.sort(String::compareTo);
+            for (String tag: closingTags) {
+                openingTags.remove(tag);
+            }
+            System.out.println("Незакрытые теги:");
+            Map<String, Long> unclosedTagsCount = openingTags.stream()
+                    .collect(Collectors.groupingBy(t -> t, Collectors.counting()));
+            System.out.println(unclosedTagsCount);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
